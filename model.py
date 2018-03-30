ca8d08526f64711a6b29ff45495c67445363593b
Yash <ysh.kant@gmail.com>
2018-03-31 00:25:31 +0530

    Fixing Bugs.

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ from IPython import display
 
 
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape=shape, mean=0.0, stddev=1.0)
+    initial = tf.truncated_normal(shape=shape, mean=0.0, stddev=0.1)
     return tf.Variable(initial)
 
 
@@ -20,7 +20,7 @@ class Model:
     def __init__(self, x, y_):
         self.in_dim = int(x.get_shape()[1])
         self.out_dim = int(y_.get_shape()[1])
-
+        self.x = x # This is a placeholder that'll be used to feed data
         # layer-1
         self.w1 = weight_variable([self.in_dim, 50])
         self.b1 = bias_variable([50])
@@ -35,7 +35,7 @@ class Model:
         # build-graph
         self.h1 = tf.nn.relu(tf.matmul(x, self.w1) + self.b1)
         self.y = tf.matmul(self.h1, self.w2) + self.b2
-        self.cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.y, labels=y_))
+        self.cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.y, labels=y_))
         self.train_step = None
         self.fisher = None
         self.star_vars = None
@@ -49,50 +49,51 @@ class Model:
     # set vanilla loss
     def vanilla_loss(self):
         self.train_step = tf.train.GradientDescentOptimizer(.1).minimize(self.cross_entropy)
-
-    # computes fisher
+        
     def compute_fisher(self, imgset, sess, num_samples=200, plot_diffs=False, disp_freq=10):
-        self.fisher = []
-        for v in self.var_list:
-            self.fisher.append(np.zeros(v.get_shape().as_list()))
+            # computer Fisher information for each parameter
 
-        probs = tf.nn.softmax(self.y)
-        class_ind = tf.to_int32(tf.multinomial(tf.log(probs), 1)[0][0])  # doubtful here, see usage somewhere!
+            # initialize Fisher information for most recent task
+            self.fisher = []
+            for v in range(len(self.var_list)):
+                self.fisher.append(np.zeros(self.var_list[v].get_shape().as_list()))
 
-        if plot_diffs:
-            fisher_prev = deepcopy(self.fisher)
-            mean_diffs = np.zeros(0)
+            # sampling a random class from softmax
+            probs = tf.nn.softmax(self.y)
+            class_ind = tf.to_int32(tf.multinomial(tf.log(probs), 1)[0][0])
 
-        for i in range(num_samples):
-            im_ind = np.random.randint(imgset.shape[0])
-            # try eval here!
-            dervs = sess.run(tf.gradients(tf.log(probs[0, class_ind]), self.var_list),
-                             feed_dict={self.x: imgset[im_ind:im_ind + 1]})
+            if(plot_diffs):
+                # track differences in mean Fisher info
+                prev_fisher = deepcopy(self.fisher)
+                mean_diffs = np.zeros(0)
 
-            for j in range(len(self.var_list)):
-                self.fisher[j] += np.square(dervs[j])
-
-            if plot_diffs:
-                if i % disp_freq == 0 and i > 0:
-                    fisher_diff = 0
-
-                    for k in range(len(self.fisher)):
-                        fisher_diff += np.sum(np.absolute(self.fisher[k] / (i + 1) - fisher_prev[k]))
-                        mean_diff = np.mean(fisher_diff)  # makes no sense to me!
+            for i in range(num_samples):
+                # select random input image
+                im_ind = np.random.randint(imgset.shape[0])
+                # compute first-order derivatives
+                ders = sess.run(tf.gradients(tf.log(probs[0,class_ind]), self.var_list), feed_dict={self.x: imgset[im_ind:im_ind+1]})
+                # square the derivatives and add to total
+                for v in range(len(self.fisher)):
+                    self.fisher[v] += np.square(ders[v])
+                if(plot_diffs):
+                    if i % disp_freq == 0 and i > 0:
+                        # recording mean diffs of F
+                        F_diff = 0
+                        for v in range(len(self.fisher)):
+                            F_diff += np.sum(np.absolute(self.fisher[v]/(i+1) - prev_fisher[v]))
+                        mean_diff = np.mean(F_diff)
                         mean_diffs = np.append(mean_diffs, mean_diff)
+                        for v in range(len(self.fisher)):
+                            prev_fisher[v] = self.fisher[v]/(i+1)
+                        plt.plot(range(disp_freq+1, i+2, disp_freq), mean_diffs)
+                        plt.xlabel("Number of samples")
+                        plt.ylabel("Mean absolute Fisher difference")
+                        display.display(plt.gcf())
+                        display.clear_output(wait=True)
 
-                    # replace previous
-                    for l in range(len(fisher_prev)):
-                        fisher_prev[v] = self.fisher[l]
-
-                    plt.plot(range(disp_freq + 1, i + 2, disp_freq), mean_diffs)
-                    plt.xlabel("Number of samples")
-                    plt.ylabel("Mean absolute Fisher difference")
-                    display.display(plt.gcf())
-                    display.clear_output(wait=True)
-
-        for i in range(len(self.fisher)):
-            self.fisher[v] /= num_samples
+            # divide totals by number of samples
+            for v in range(len(self.fisher)):
+                self.fisher[v] /= num_samples
 
     def star(self):
         # used for saving optimal weights after most recent task training
@@ -108,7 +109,7 @@ class Model:
                 sess.run(self.var_list[v].assign(self.star_vars[v]))
 
     # having doubts with the graph here! I think let's try tensor-board maybe :D
-    def ewc_loss(self, lam):
+    def set_ewc_loss(self, lam):
         if not hasattr(self, "ewc_loss"):
             self.ewc_loss = self.cross_entropy
 
